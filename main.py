031136eac82c093ce9b8146d7ff10a1f28b26d12
Barry Bridgens <barry@bridgens.me.uk>
2023-11-02 12:57:27 +0000

    First working version

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # inspired by YouTube video from Steve's Coding Lab
 
-import pygame,sys
+import pygame, sys, time, copy
 
 from gol_util import *
 
@@ -23,17 +23,26 @@ if (__name__ == "__main__"):
 
     grid_state = [[DEAD for _ in range(TOTAL_ROWS_COLS)]
                   for _ in range(TOTAL_ROWS_COLS)]
+    new_state = copy.deepcopy(grid_state)
     
     pygame.init()
     screen = pygame.display.set_mode((SC_WIDTH_HEIGHT, SC_WIDTH_HEIGHT))
 
-    button = False;
+    button = False
+    run = False
+    stop = False
+    running = False
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    run = True
+                elif event.key == pygame.K_s:
+                    stop = True
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 button = True;
             elif event.type == pygame.MOUSEBUTTONUP:
@@ -45,16 +54,82 @@ if (__name__ == "__main__"):
 
                 if grid_state[row][col] == DEAD:
                     grid_state[row][col] = ALIVE
-                else:
-                    grid_state[row][col] = DEAD
+                #else:
+                #    grid_state[row][col] = DEAD
 
-                
-            
 
         screen.fill(COLOUR_BG)
         draw_grid()
+
+        # Game of Life Logic
+        if run:
+            running = True
+            run = False
+        elif stop:
+            running = False
+            stop = False
+        
+        if running:
+            for x in range(TOTAL_ROWS_COLS):
+                for y in range(TOTAL_ROWS_COLS):
+                    # Count neighbours
+                    neighbours = 0
+                    # Left
+                    if x > 0:
+                        if grid_state[x - 1][y] == ALIVE:
+                            neighbours = neighbours + 1
+                    # Right
+                    if x < (TOTAL_ROWS_COLS - 1):
+                        if grid_state[x + 1][y] == ALIVE:
+                            neighbours = neighbours + 1
+                    # Above
+                    if y < (TOTAL_ROWS_COLS - 1):
+                        if grid_state[x][y + 1] == ALIVE:
+                            neighbours = neighbours + 1
+                    # Below
+                    if y > 0:
+                        if grid_state[x][y - 1] == ALIVE:
+                            neighbours = neighbours + 1
+                    # Down and left
+                    if (x > 1) and (y > 0):
+                        if grid_state[x - 1][y - 1] == ALIVE:
+                            neighbours = neighbours + 1
+                    # Up and left
+                    if (x > 1) and (y < (TOTAL_ROWS_COLS - 1)):
+                        if grid_state[x - 1][y + 1] == ALIVE:
+                            neighbours = neighbours + 1
+                    # Up and right
+                    if (x < (TOTAL_ROWS_COLS - 1)) and (y < (TOTAL_ROWS_COLS - 1)):
+                        if grid_state[x + 1][y + 1] == ALIVE:
+                            neighbours = neighbours + 1
+                    # Down and right
+                    if (x < (TOTAL_ROWS_COLS - 1)) and (y > 0):
+                        if grid_state[x + 1][y - 1] == ALIVE:
+                            neighbours = neighbours + 1
+
+                    # Live / Die / Born Logic
+                    if grid_state[x][y] == ALIVE:
+                        if neighbours < 2:
+                            new_state[x][y] = DEAD
+                        elif ((neighbours == 2) or (neighbours == 3)):
+                            new_state[x][y] = ALIVE
+                        elif neighbours > 3:
+                            new_state[x][y] = DEAD
+                        else:
+                            new_state[x][y] = DEAD
+                    elif grid_state[x][y] == DEAD:
+                        if neighbours == 3:
+                            new_state[x][y] = ALIVE
+                    else:
+                        new_state[x][y] = DEAD
+
+            grid_state = copy.deepcopy(new_state)
+
         
         display_cells(grid_state)
             
         pygame.display.update()
+
+        if running:
+            time.sleep(1)
 
