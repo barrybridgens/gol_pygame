aad1768fad36509373e1aa724194f3f251aa5062
Barry Bridgens <barry@bridgens.me.uk>
2023-11-02 11:00:59 +0000

    Initial cell state setting

--- gol_util.py
+++ gol_util.py
@@ -1,12 +1,16 @@
-# Utility functions for Game of life
+# Utility data and functions for Game of life
+
 
 SC_WIDTH_HEIGHT = 800
 
 COLOUR_BG = (40, 40, 40)
 COLOUR_GRID = (80, 80, 80)
+COLOUR_ALIVE = (240, 240, 240)
 
 TOTAL_ROWS_COLS = 80
 CELL_SIZE = SC_WIDTH_HEIGHT // TOTAL_ROWS_COLS
 
-def draw_grid():
-    pass
+DEAD = 0
+ALIVE = 1
+
+
--- main.py
+++ main.py
@@ -1,21 +1,60 @@
 # Game of Life
 
+# inspired by YouTube video from Steve's Coding Lab
+
 import pygame,sys
 
 from gol_util import *
 
-pygame.init()
-screen = pygame.display.set_mode((SC_WIDTH_HEIGHT, SC_WIDTH_HEIGHT))
+def draw_grid():
+    for row in range(TOTAL_ROWS_COLS):
+        for col in range(TOTAL_ROWS_COLS):
+            rect = pygame.Rect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+            pygame.draw.rect(screen, COLOUR_GRID, rect, 1)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+def display_cells(cells):
+    for x in range(TOTAL_ROWS_COLS):
+        for y in range(TOTAL_ROWS_COLS):
+            if cells[x][y] == ALIVE:
+                rect = pygame.Rect(y * CELL_SIZE, x * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+                pygame.draw.rect(screen, COLOUR_ALIVE, rect, 0)    
 
-    screen.fill(COLOUR_BG)
+if (__name__ == "__main__"):
 
+    grid_state = [[DEAD for _ in range(TOTAL_ROWS_COLS)]
+                  for _ in range(TOTAL_ROWS_COLS)]
+    
+    pygame.init()
+    screen = pygame.display.set_mode((SC_WIDTH_HEIGHT, SC_WIDTH_HEIGHT))
 
+    button = False;
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                button = True;
+            elif event.type == pygame.MOUSEBUTTONUP:
+                button = False
+            elif (event.type == pygame.MOUSEMOTION) and button:
+                pos = pygame.mouse.get_pos()
+                col = pos[0] // CELL_SIZE
+                row = pos[1] // CELL_SIZE
 
-    pygame.display.update()
+                if grid_state[row][col] == DEAD:
+                    grid_state[row][col] = ALIVE
+                else:
+                    grid_state[row][col] = DEAD
+
+                
+            
+
+        screen.fill(COLOUR_BG)
+        draw_grid()
+        
+        display_cells(grid_state)
+            
+        pygame.display.update()
 
